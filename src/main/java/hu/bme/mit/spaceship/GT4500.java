bfb244c89d10de7ff97bf9b6e7446dee86d2b43e
kovacsnoi <66200516+kovacsnoi@users.noreply.github.com>
2023-04-21 13:33:47 +0200

    fix

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -37,6 +37,8 @@ public class GT4500 implements SpaceShip {
   public boolean fireTorpedo(FiringMode firingMode) {
 
     boolean firingSuccess = false;
+    boolean firstSuccess = false;
+    boolean secondSuccess = false;
 
     switch (firingMode) {
       case SINGLE:
@@ -77,9 +79,11 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
-        //TODO implement feature
-
+        firstSuccess = primaryTorpedoStore.fire(1);
+        secondSuccess = secondaryTorpedoStore.fire(1);
+        if (firstSuccess  || secondSuccess){
+          firingSuccess = true;
+        }
         break;
     }
 
